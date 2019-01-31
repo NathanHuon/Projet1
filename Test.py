dc51b5c874b42a6684988b90489297787dcf7bfd
NathanHuon <huonnat@gmail.com>
2019-01-31 20:48:54 +0100

    Mon promier projet

---
+++ Test.py
@@ -0,0 +1,9 @@
+
+vars = 5 
+print ("Hello "+ str(vars))
+for i in range(0,10):
+    print("i vaut:"+str(i))
+    for n in range(5,20):
+        z=i+n
+        print("z vaut:"+str(z))
+    print("-----------------")

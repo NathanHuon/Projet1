e77b57b89ec036e3e409ed9e714d868eeecbfe1d
NathanHuon <huonnat@gmail.com>
2019-02-04 14:02:58 +0100

    Dernière modification

--- Firstproject.py
+++ Firstproject.py
@@ -6,7 +6,6 @@ def digit1(z):
         try:
             x = int(x)
             return x
-            break
         except:
             print ("Un prix est un nombre {} n'est pas un nombre".format(x))
             x = input("Un prix : ")
@@ -16,20 +15,37 @@ random_number = digit1("Entrer les dernier prix possible : ")
 
 just_price = randint(1, random_number)
 
-condition = 0
+print("""
+   _           _            
+  (_)_   _ ___| |_ ___      
+  | | | | / __| __/ _ \     
+  | | |_| \__ \ ||  __/     
+ _/ |\__,_|___/\__\___|     
+|__/                        
+            ___      _      
+           / _ \_ __(_)_  __
+          / /_)/ '__| \ \/ /
+         / ___/| |  | |>  < 
+         \/    |_|  |_/_/\_\
+         
+""")
+
+
+
 trys = 0
-while condition == 0:
+
+while(True):
  
     user_price = digit1("Entrer un prix : ")
     trys = trys + 1
     if user_price == just_price:
-        condition =+ 1
-        
+        break
  
-    elif user_price > just_price:
-        print("C'est moins")
-       
-    elif user_price < just_price:
-        print("C'est plus")
+    else:
+        if user_price > just_price:
+            print("C'est moins")
+        else:
+            if user_price < just_price:
+                print("C'est plus")
 
 print("C'est fini." + " Le prix est {}.".format(just_price) + "Tu as fais {} essaies".format(trys))
